5670e53b2859cd327da53a706289a4ad8f7c7fc3
anwerriahi <anwarriahi.riahi@gmail.com>
2023-07-30 20:18:59 +0100

    npm run build

--- src/Footer.js
+++ src/Footer.js
@@ -13,6 +13,19 @@ export function Footer({ handleSpeed, speed, handlePlay, playPause }) {
       <p className="sort-speed">
         Sorting Interval Speed: {speed === 1000 ? "1 s" : speed + " ms"}
       </p>
+      <span
+        style={{
+          display: "block",
+          fontSize: "12px",
+          backgroundColor: "bisque",
+          width: "10rem",
+          margin: "0 auto",
+          borderRadius: "10px",
+          padding: "5px",
+        }}
+      >
+        Development in progress...
+      </span>
       <button className="sort" onClick={handlePlay}>
         {playPause ? "Stop Sorting" : "Sort"}
       </button>
